a40c3c6385ff6a30fc586825d638ae61876c4bdd
Doug Shamoo <doug.shamoo@gmail.com>
2015-10-14 21:49:12 -0700

    (feat) Implement Braintree API on server [Closes #10]
    add braintree js sdk and implement basic token gen and transaction

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  braintree: {
+    merchantId: 'jmm6pqzfnp9nsbzt',
+    publicKey: 'k348f7tm59j6f8dm',
+    privateKey: 'cc4b42f028bc5839bacd94b9f9ecc4b5',
+  },
+};
--- server/server.js
+++ server/server.js
@@ -2,13 +2,58 @@
 var express = require('express');
 var app = express();
 
-// attach middleware, for example: 
-//  app.use(bodyParser.json());
+var braintree = require('braintree');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var util = require('util');
+var config = require('./config.js');
+
+// Instantiate the braintree gateway.
+// Note: Must change these values for production
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: config.braintree.merchantId,
+  publicKey: config.braintree.publicKey,
+  privateKey: config.braintree.privateKey,
+});
+
+// attach middleware
 app.use(express.static(__dirname + '/../client'));
 
+// Send a client token to client
+app.get('/client_token', function(req, res) {
+  gateway.clientToken.generate({}, function(err, response) {
+    res.send(response.clientToken);
+  });
+});
 
-// add routes to handle GETs and POSTs
+// Receive a payment method nonce from client
+app.post('/checkout', jsonParser, function(req, res) {
+  var transaction = req.body;
 
+  // TODO: replace with req.body.payment_method_nonce after testing
+  // var nonce = transaction.payment_method_nonce;
+  // For a list of static test nonces, visit:
+  //  https://developers.braintreepayments.com/javascript+node/reference/general/testing
+  var nonce = 'fake-valid-nonce';
+
+  // use payment method nonce here, test example below
+  gateway.transaction.sale({
+    amount: transaction.amount || '5.00', // TODO: remove default value for testing
+
+    // TODO: specify merchantAccountId of the artist,
+    // defaults to starvingartists default merchantAccountId if not specified
+
+    paymentMethodNonce: nonce,
+  }, function(err, result) {
+    if (err) {
+      throw error;
+    }
+
+    console.log(util.inspect(result));
+    res.json(result);
+  });
+});
 
 // export the express app to be used in index.js
 module.exports = app;
