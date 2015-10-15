e3aa26131304ace297b6d7020ace9cbde9d66340
Rod Machen <mail@rodmachen.com>
2015-10-15 13:45:02 -0500

    Add server/config.js

--- db/config.js
+++ db/config.js
@@ -1,11 +1,32 @@
-var Sequelize = require('sequelize');
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize');
+  var sequelize = null;
 
-var sequelize = new Sequelize('development', 'Username', null, {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    timestamps: false,
-  },
-});
+  if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize('example-app-db', 'root', null)
+  }
 
-module.exports.database = sequelize;
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    User:      sequelize.import(__dirname + '/user') 
+    // add your other models here
+  }
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
+}
+
+module.exports = global.db;
