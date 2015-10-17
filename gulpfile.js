ee807b3278b5bd2af5e09802d65b1427d38d1db0
Doug Shamoo <doug.shamoo@gmail.com>
2015-10-16 21:45:53 -0700

    (test) Add karma/mocha-chai, basic AppCtrl spec template [Closes #13]
    - `gulp test` runs tests
    - `gulp check` runs linter, then tests
    - added dev dependencies: chai, jshint-stylish, karma, karma-chai,
    karma-mocha, karma-mocha-reporter, karma-phantomjs-launcher, mocha,
    phantomjs.
    - added bower dependencies: angular-mocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var Server = require('karma').Server;
 
 gulp.task('lint', function() {
   return gulp.src(['./server/**/*.js', './client/**/*.js', '!./client/lib/**/*.js', './*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('jshint-stylish'));
 });
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function(done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+  }, done).start();
+});
+
+gulp.task('check', ['lint', 'test']);
 
 gulp.task('default', function() {
   // place code for your default task here
