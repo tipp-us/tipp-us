cc4c50f2fc337ff3d6723e6795b203a5ca1b2133
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-17 13:12:03 -0400

    (fix) change img src to ng-src and minor fix in artistDisplay directive

--- client/app.js
+++ client/app.js
@@ -117,24 +117,24 @@ app.directive('artistDisplay', ['$rootScope', '$state', function($scope, $state)
     controller: ['$http', function($http) {
       if(!$scope.artist) {
         $state.go('^.home');
+      } else {
+        var self = this;
+        this.artistInfo = {};
+        // as of 10-16, server still responding with dummy data
+        $http.post('/artist', {artistId: $scope.artist.id}).success(function(data) {
+          console.log('in artistDisplay. $scope.artist is...');
+          console.log($scope.artist);
+          console.log(data);
+          self.info = data;
+        });
+        this.click = function(artist) {
+          // console.log(artist);
+        };
+        this.go = function() {
+          var art = $scope.artist
+          window.location.href = ("http://maps.google.com/maps?q="+art.position.lat+","+art.position.long+"+(My+Point)&z=14&ll="+art.position.lat+","+art.position.long);
+        };
       }
-      var self = this;
-      this.artistInfo = {};
-      // as of 10-16, server still responding with dummy data
-      $http.post('/artist', {artistId: $scope.artist.id}).success(function(data) {
-        console.log('in artistDisplay. $scope.artist is...');
-        console.log($scope.artist);
-        console.log(data);
-        self.info = data;
-      });
-      this.click = function(artist) {
-        // console.log(artist);
-      };
-      this.go = function() {
-        var art = $scope.artist
-        window.location.href = ("http://maps.google.com/maps?q="+art.position.lat+","+art.position.long+"+(My+Point)&z=14&ll="+art.position.lat+","+art.position.long);
-      }
-
     }],
     controllerAs: 'selectedArtist'
   };
