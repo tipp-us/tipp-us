6de03498d58e5da7b290603161bc453b8b728e2f
Rod Machen <mail@rodmachen.com>
2015-10-19 17:36:04 -0500

    update config

--- db/config.js
+++ db/config.js
@@ -4,12 +4,13 @@ if (!global.hasOwnProperty('db')) {
 
   if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
+    // var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
     sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true, //false
+      // port:     match[4],
+      // host:     match[3],
+      logging:  false,
     });
   } else {
     // the application is executed on the local machine ...
