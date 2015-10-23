7e08095534cd09a2b199605d78e6516b8c3f5344
unknown <joe@josephbrumaghim.com>
2015-10-23 09:41:15 -0400

    (refactor) Refactored directives out of app.js

---
+++ client/directives/artistdisplay.js
@@ -0,0 +1,29 @@
+app.directive('artistDisplay', ['$rootScope', '$state', function($scope, $state){
+  return {
+    restrict: 'E',
+    templateUrl: 'artistDisplay.html',
+    controller: ['$http', function($http) {
+      if(!$scope.artist) {
+        $state.go('^.home');
+      } else {
+        var self = this;
+        this.artistInfo = {};
+        // as of 10-16, server still responding with dummy data
+        $http.post('/artist', {artistId: $scope.artist.id}).success(function(data) {
+          console.log('in artistDisplay. $scope.artist is...');
+          console.log($scope.artist);
+          console.log(data);
+          self.info = data;
+        });
+        this.click = function(artist) {
+          // console.log(artist);
+        };
+        this.go = function() {
+          var art = $scope.artist;
+          window.location.href = ("http://maps.google.com/maps?q="+art.position.lat+","+art.position.long+"+(My+Point)&z=14&ll="+art.position.lat+","+art.position.long);
+        };
+      }
+    }],
+    controllerAs: 'selectedArtist'
+  };
+}]);
