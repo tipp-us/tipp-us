3bf625f9c2014f0d13fc73b4108086149e432a7c
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-23 23:45:43 -0400

    (feat) 'gulp build' concats, uglifys, then runs node index.js

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: node node_modules/gulp/bin/gulp build
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var shell = require('gulp-shell');
 var Server = require('karma').Server;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var run = require('gulp-run');
 
 gulp.task('lint', function() {
   return gulp.src(['./server/**/*.js', './client/**/*.js', '!./client/lib/**/*.js', './*.js'])
@@ -26,6 +29,19 @@ gulp.task('presdb', shell.task('node db/dummydb-pres.js'));
 gulp.task('dropdb', shell.task('node db/dropdb.js'));
 
 gulp.task('check', ['lint', 'test']);
+
+gulp.task('uglyCat', function() {
+  return gulp.src(['./client/app.js', './client/controllers/*.js', './client/directives/*.js'])
+    .pipe(concat('starvingall.js'))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('./client/dist/'));
+});
+
+gulp.task('nodeIndex', function() {
+  run('node index.js').exec();
+});
+
+gulp.task('build', ['uglyCat', 'nodeIndex']);
 
 gulp.task('default', function() {
   // place code for your default task here
