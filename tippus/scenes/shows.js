02ac2f710a82a43c91760a962f13219d58a5d22b
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-30 20:25:14 -0400

    (feat) add show layout

--- tippus/scenes/shows.js
+++ tippus/scenes/shows.js
@@ -1,20 +1,68 @@
 'use strict';
 
 var React = require('react-native');
+var MK = require('react-native-material-kit');
+
 var {
   StyleSheet,
   Text,
   View,
+  TextInput,
 } = React;
+
+const {
+  MKButton,
+  MKColor,
+} = MK;
+
+const UseCurrentLocationButton = MKButton.accentColoredButton()
+  .withText('USE CURRENT LOCATION')
+  .withOnPress(() => {
+    console.log("Hi, it's a colored button!");
+  })
+  .build();
+
+const AddShowButton = MKButton.accentColoredButton()
+  .withText('ADD SHOW')
+  .withOnPress(() => {
+    console.log("Hi, it's a colored button!");
+  })
+  .build();
 
 var Shows = React.createClass({
   render: function() {
     return (
       <View style={pageStyle.container}>
+
         <Text style={pageStyle.welcome}>
-          Form to fill out show info
+          Shows
         </Text>
+
+        <TextInput ref={component => this._textInput = component} 
+            style={{height: 50, width: 300, borderWidth: 1, borderColor: '#888888 '}}
+            placeholder="Venue"></TextInput>
+
+        <TextInput ref={component => this._textInput = component} 
+            style={{height: 50, width: 300, borderWidth: 1, borderColor: '#888888 '}}
+            placeholder="Lat"></TextInput>
+
+        <TextInput ref={component => this._textInput = component} 
+            style={{height: 50, width: 300, borderWidth: 1, borderColor: '#888888 '}}
+            placeholder="Long"></TextInput>
+
+        <UseCurrentLocationButton/>
+
+        <TextInput ref={component => this._textInput = component} 
+            style={{height: 50, width: 300, borderWidth: 1, borderColor: '#888888 '}}
+            placeholder="Start Time"></TextInput>
+
+        <TextInput ref={component => this._textInput = component} 
+            style={{height: 50, width: 300, borderWidth: 1, borderColor: '#888888 '}}
+            placeholder="End Time"></TextInput>
+
+        <AddShowButton/>
       </View>
+
     );
   }
 });
